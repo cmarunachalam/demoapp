9800a32e4e4bcaa281325b3ed07bb0ab0c8e7282
cmarunachalam <104051892+cmarunachalam@users.noreply.github.com>
2023-02-02 15:02:48 +0530

    Update DemoController.java

--- src/main/java/com/javadeveloperzone/DemoController.java
+++ src/main/java/com/javadeveloperzone/DemoController.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class DemoController {
     @RequestMapping(value = "hello")
     public String hello(){
-        System.out.println("==Demo==");
+        System.out.println("Demo ");
         return "demo example - "+new Date();
     }
 }
