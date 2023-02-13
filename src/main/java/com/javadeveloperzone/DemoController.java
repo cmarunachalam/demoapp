ba4abe427a0dc4827c054ceb4f8cf827654f7896
murali <c_marunachalam@cloudbees.com>
2023-02-13 12:47:34 +0530

    added the method signature

--- src/main/java/com/javadeveloperzone/DemoController.java
+++ src/main/java/com/javadeveloperzone/DemoController.java
@@ -1,18 +1,19 @@
 package com.javadeveloperzone;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
 @RestController
 public class DemoController {
-    @RequestMapping(value = "home")
+    @RequestMapping(value = "home", method = RequestMethod.GET)
     public String home(){
         return "Home-"+new Date();
     }
     
-    @RequestMapping(value = "services")
+    @RequestMapping(value = "services", method = RequestMethod.GET)
     public String services(){
         return "Services";
     }
