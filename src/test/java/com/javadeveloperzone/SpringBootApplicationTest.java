1ae2b66b6ab99bc03cc7ce9948a85526fef9b8d8
murali <c_marunachalam@cloudbees.com>
2023-02-22 20:35:30 +0530

    update code coverage

---
+++ src/test/java/com/javadeveloperzone/SpringBootApplicationTest.java
@@ -0,0 +1,12 @@
+package com.javadeveloperzone;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootApplicationTest {
+    @Test
+    public void main() {
+        SpringBootApplication.main(new String[] {});
+    }
+}
